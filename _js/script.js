26763eee83f339b54a8a22e01b2fd54242013211
Gabriel <gabriel.lopesblue13@gmail.com>
2025-04-23 20:30:25 -0300

    adicao da funcionalidade de links

---
+++ _js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("portfolio").addEventListener("click", function() {
+    window.open("https://github.com/GalSal0967/Portfolio", "_blank");
+});
+
+document.getElementById("calculadora_imc").addEventListener("click", function() {
+    window.open("https://github.com/GalSal0967/IMC", "_blank");
+});
+
+document.getElementById("cafe").addEventListener("click", function() {
+    window.open("https://github.com/GalSal0967/CoffeCups", "_blank");
+});
